c616e123bf1e992628c0441d0b9f83f8d8e1cb0b
Nicolò Rossi <35870645+nikox96@users.noreply.github.com>
2018-02-19 09:45:42 +0100

    aggiunto prefisso tabella

--- app/models/product.js
+++ app/models/product.js
@@ -4,7 +4,7 @@ var Product = {};
 
 Product.find = function find(cprod, callback) {
     console.log("ricerca codice prodotto " + cprod);
-    var query = "SELECT * FROM prodotti WHERE ccod = " + cprod;
+    var query = "SELECT * FROM portale.prodotti WHERE ccod = " + cprod;
     console.log(query);
     db.query(query
         , function (queryErr, queryRes) {
@@ -26,7 +26,7 @@ Product.list = function list(ccodda, ccoda, sven, xgrp, xprod, callback) {
     console.log("sven " + sven);
     console.log("xgrp " + xgrp);
     console.log("xprod" + xprod);
-    var query = "SELECT * FROM prodotti WHERE ccod > " + (ccodda && ccodda !== '' ? ccodda : 0) + " AND ccod < " + (ccoda && ccoda !== '' ? ccoda : 999999)
+    var query = "SELECT * FROM portale.prodotti WHERE ccod > " + (ccodda && ccodda !== '' ? ccodda : 0) + " AND ccod < " + (ccoda && ccoda !== '' ? ccoda : 999999)
         + " AND xgrp LIKE '%" + (xgrp && xgrp !== '' ? xgrp : '') + "%' AND xdesc LIKE '%" + (xprod && xprod ? xprod : '')
         + "%' AND sven LIKE '%" + (sven && sven !== '' ? sven : '') + "%'";
     console.log(query);
@@ -45,7 +45,7 @@ Product.list = function list(ccodda, ccoda, sven, xgrp, xprod, callback) {
 
 Product.getSven = function (callback) {
     console.log("ricerca tipo vendita");
-    var query = "SELECT sven FROM prodotti group by sven";
+    var query = "SELECT sven FROM portale.prodotti group by sven";
     console.log(query);
     db.query(query
         , function (queryErr, queryRes) {
@@ -58,7 +58,7 @@ Product.getSven = function (callback) {
 
 Product.getXgrp = function (callback) {
     console.log("ricerca categoria prodotto");
-    var query = "SELECT xgrp FROM prodotti group by xgrp";
+    var query = "SELECT xgrp FROM portale.prodotti group by xgrp";
     console.log(query);
     db.query(query
         , function (queryErr, queryRes) {
