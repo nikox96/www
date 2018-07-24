85bdacedd426b1a860a7348b7c06bce42688c7af
Nicolò Rossi <35870645+nikox96@users.noreply.github.com>
2018-07-24 09:14:57 +0200

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
+   caches.open('portaleInnovazioni').then(function(cache) {
      return cache.addAll([
        '/offline',
        '/client-list',
