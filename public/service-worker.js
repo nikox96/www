c0bb90b5301342f001b0a82cf27732f44da90a7c
Nicolò Rossi <35870645+nikox96@users.noreply.github.com>
2018-07-24 08:48:52 +0200

    WebApk: salvo cache per lista clienti e prodotti

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,19 @@
+importScripts('/cache-polyfill.js');
 
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/offline',
+       '/client-list',
+       '/product-list',
+       'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+       '/material-design-icons/iconfont/material-icons.css',
+       'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+       'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+       'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
+     ]);
+   })
+ );
+});
