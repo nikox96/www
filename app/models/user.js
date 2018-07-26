882a1cf0d2649e49f96fc440daf33cd8681dfe61
Nicolò Rossi <35870645+nikox96@users.noreply.github.com>
2018-07-26 14:00:15 +0200

    debug

--- app/models/user.js
+++ app/models/user.js
@@ -51,8 +51,11 @@ User.login = function login(cage, xpwd, callback) {
                 queryRes = (queryRes.rows && queryRes.rows.length > 0 ? queryRes.rows : queryRes);
                 if (queryRes.length > 0) {
                     console.log('sql password: ' + queryRes[0].xpwd);
-                    console.log('html passord: ' + xpwd);
+                    console.log('html password: ' + xpwd);
+                    console.log('html password (tostring): ' + xpwd.toString());
                     bcrypt.compare(xpwd.toString(), mysql.escape(queryRes[0].xpwd), function (bcryptErr, bcryptRes) {
+                        console.log('escaped: ' + mysql.escape(queryRes[0].xpwd));
+                        console.log('not escaped: ' + queryRes[0].xpwd);
                         console.log('compare result: ' + bcryptRes);
                         console.log('compare error: ' + bcryptErr);
                         if (bcryptErr || !(bcryptRes)) {
