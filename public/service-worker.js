78c41c004ce1f640a3543b352ea7e1a564a5e1e6
Nicolò Rossi <35870645+nikox96@users.noreply.github.com>
2018-07-26 10:29:21 +0200

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,13 +2,15 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('portaleInnovazioni').then(function(cache) {
      return cache.addAll([
-       '/offline',
+       /*'/offline',*/
        '/client-list',
-       '/product-list',
+       /*'/product-list',*/
        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
        '/material-design-icons/iconfont/material-icons.css',
        'https://code.jquery.com/jquery-3.2.1.slim.min.js',
        'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+       'https://use.fontawesome.com/025507bc43.js',
+       'https://use.fontawesome.com/releases/v5.2.0/css/all.css',
        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
      ]);
    })
